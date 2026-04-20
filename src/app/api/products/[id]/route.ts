f49595ffe8eaf2f6d70a2f2a0493d3d3808f6050
AOrdaz <ja.ordaz.it@gmail.com>
2026-04-20 11:19:21 -0600

    actualizar put y delete

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,32 +1,20 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 //import { PrismaClient } from '@/generated/prisma/client';
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export async function PUT(request: { json: () => any }, { params }: any) {
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
   try {
-    const { id } = params
+    const { id } = await params
     const body = await request.json()
-    const { name, description, price, stock, imageUrl } = body
-    
-    const product = await prisma.product.update({
-      where: { id: parseInt(id) },
-      data: { 
-        name, 
-        description, 
-        price: parseFloat(price), 
-        stock: parseInt(stock), 
-        imageUrl 
-      }
-    })
+    const product = await prisma.product.update({ where: { id: parseInt(id) }, data: body })
     return NextResponse.json(product)
   } catch (error) {
-    /*return NextResponse.json(
-      { error: 'Error al actualizar producto' },
-      { status: 500 }
-    )*/
-    console.error('Error en update /api/products:', error)
+    console.error('Error en update /api/products/[id]:', error)
     return Response.json(
       { error: String(error) },
       { status: 500 }
@@ -34,19 +22,16 @@ export async function PUT(request: { json: () => any }, { params }: any) {
   }
 }
 
-export async function DELETE(request: any, { params }: any) {
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
   try {
-    const { id } = params
-    await prisma.product.delete({ 
-      where: { id: parseInt(id) } 
-    })
-    return NextResponse.json({ message: 'Producto eliminado' })
+    const { id } = await params
+    await prisma.product.delete({ where: { id: parseInt(id) } })
+    return NextResponse.json({ ok: true })
   } catch (error) {
-    /*return NextResponse.json(
-      { error: 'Error al eliminar producto' },
-      { status: 500 }
-    )*/
-    console.error('Error en delete /api/products:', error)
+    console.error('Error en delete /api/products/[id]:', error)
     return Response.json(
       { error: String(error) },
       { status: 500 }
